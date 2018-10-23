c4ed7b61494c86f7c798d3e9db8bfc3ad549e526
pier <nmall843r>
2018-10-23 09:22:28 +0200

    updated with safety envelope

--- agents/simple_agent.py
+++ agents/simple_agent.py
@@ -1,12 +1,18 @@
 import gym_minigrid
 import gym
 
+from envelopes.patterns.envelopes_light import  *
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.ppo2 import PPO2
 
+
 env = gym.make('MiniGrid-DirtWatLightExp-9x9-v0')
+
+env = SafetyEnvelope(env)
+
 env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
+
 
 model = PPO2(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=10000)
