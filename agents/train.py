02965ffe80dde4b0b6dade8de359cdbdcfdea6fb
pier <nmall843r>
2018-12-10 22:44:50 +0100

    added config

--- agents/train.py
+++ agents/train.py
@@ -5,25 +5,29 @@ import time
 from envelopes.patterns.envelopes_light import *
 from stable_baselines.bench import Monitor
 
+from configurations import config_grabber as cg
+
 from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy
 from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines import PPO2
 
 from agents.utils_functions import *
 
+config = cg.Configuration.grab()
+
 # Create log dir
 log_dir = "./log/"
 os.makedirs(log_dir, exist_ok=True)
 
-env_id = "MiniGrid-LavaCrossingS9N1-v0"
-n_timesteps = 250000
+env_id = config.env_name
+n_timesteps = config.max_num_steps
 
 # multiprocess environment
-n_cpu = 4
+n_cpu = config.n_cpu
 env = SubprocVecEnv([lambda: gym.make(env_id) for i in range(n_cpu)])
 
 model = PPO2(MlpLstmPolicy, env, verbose=1)
-model.learn(total_timesteps=2500000)
+model.learn(total_timesteps=n_timesteps)
 model.save("trained_models/MiniGrid")
 
 del model # remove to demonstrate saving and loading
