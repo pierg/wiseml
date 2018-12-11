112db72378ba75e000601b65fbdbd636ee132b38
pier <nmall843r>
2018-12-11 14:53:33 +0100

    added launch_script

--- launch_script.sh
+++ launch_script.sh
@@ -1,3 +1,62 @@
 #!/usr/bin/env bash
-echo "Ciao"
-bash
+
+# Sets the main.json as default, if the -c is specifed
+# it will use that as config file.
+configuration_file="main.json"
+
+n_timesteps=-1
+start_training=false
+verbose=false
+
+while getopts c:s:vt opt; do
+    case ${opt} in
+        c)
+            configuration_file=${OPTARG}
+            start_training=true
+            ;;
+        s)
+            n_timesteps=${OPTARG}
+            start_training=true
+            ;;
+        v)
+            verbose=true
+            start_training=true
+            ;;
+        t)
+            start_training=true
+            ;;
+    esac
+done
+
+
+if $start_training ; then
+        echo "...environment name is..."
+        echo $configuration_file
+
+        if [ $configuration_file -eq "main.json" ]; then
+            echo "using default configuration file: $configuration_file"
+            cd ./configurations
+        else
+            echo "...updating selected configuration file..."
+            cd ./configurations
+            echo "using configuration file: $configuration_file"
+            yes | cp -rf $configuration_file "main.json"
+        fi
+        cd ..
+
+        echo "...setting up python environment..."
+        PYTHONPATH=../gym-minigrid/:../gym-minigrid/gym_minigrid/:../baselines/:./configurations:./:$PYTHONPATH
+        export PYTHONPATH
+
+        echo "...launching the training..."
+        if $verbose ; then
+            python3 agents/train.py --n_timesteps=$n_timesteps --verbose
+        else
+            python3 agents/train.py --n_timesteps=$n_timesteps
+        fi
+else
+        echo "...accessing the container without launching anything..."
+        bash
+fi
+
+
